9cef7990548a199f7e5f62f4e0b8edda4ec7dbca
mohamed magdy <moh.mag.ali@gmail.com>
2020-08-06 23:10:52 +0300

    Starter

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 //create module by create self invoked anynoums function
 var budgetController = (function (){
-    var a = 23;
-
-    function add(x){
-        return x+a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+    //TODO
 })();
 
 var UIController = (function(){
-    // do some code
+    //TODO
 })();
 
 
 // create anther controller to can access budgetController and UIController
 var controller = (function(bdgtCtrl,uiCtrl){
-    var z = bdgtCtrl.publicTest(30);
-
-    return {
-        method1: function(){
-            console.log(z);
-        }
-    }
+    //TODO
 })(budgetController, UIController);
